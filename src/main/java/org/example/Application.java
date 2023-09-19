419686b1322ed34bcd75dfd535e71178be22675b
VotreNom <shawanbarua70@gmail.com>
2023-09-19 18:21:26 +0200

    add token function

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,21 +2,47 @@ package org.example;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import org.jetbrains.annotations.NotNull;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
+
 public class Application extends ListenerAdapter {
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+
         if (event.getMessage().getMentions().getMembers().contains(event.getGuild().getSelfMember())) {
             List<Member> mentionedMembers = event.getMessage().getMentions().getMembers();
             Message message = event.getMessage();
-            if (mentionedMembers.size() == 1 && mentionedMembers.get(0).equals(event.getGuild().getSelfMember())) {
-                event.getChannel().sendMessage("Bonjour, je suis le bot !").queue();
+            String Message = event.getMessage().getContentRaw();
+            List<String> newMessage = arrayToken(Message);
+
+            if (mentionedMembers.size() == 1 && mentionedMembers.get(0).equals(event.getGuild().getSelfMember()) && !event.getAuthor().isBot()) {
+                event.getChannel().sendMessage("Bonjour, je suis le bot !" + newMessage.get(0)).queue();
             }
+
         }
 
+
+    }
+    public  void onSlashCommandInteraction(@NotNull SlashCommandInteraction event){
+        if (event.getName().equals("mob")){
+            event.reply("your mob");
+        }
+    }
+    public  static ArrayList<String> arrayToken(String message){
+        StringTokenizer st = new StringTokenizer(message);
+        List<String> newMessage = new ArrayList<>();
+        while (st.hasMoreTokens()){
+            for (int i = 0; i < newMessage.size();i++) newMessage.set(i, st.nextToken());
+        }
+        return (ArrayList<String>) newMessage;
     }
     }
 
--- src/main/java/org/example/DiscordBot.java
+++ src/main/java/org/example/DiscordBot.java
@@ -4,15 +4,21 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.entities.Guild;
+
 public class DiscordBot {
 
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().load();
         String token = dotenv.get("DISCORD_BOT_TOKEN");
-        JDA jdaBuilder =  JDABuilder.createDefault(token)
+        JDA jdaBuilder = JDABuilder.createDefault(token)
                 .setActivity(Activity.listening("your name"))
-                .addEventListeners(new Application(),new Botcommand()).build();
+                .addEventListeners(new Application()).build();
+
+       // Guild guild = jdaBuilder.getGuildById("1152207053473988658");
+        jdaBuilder.upsertCommand("mob","mob is good").setGuildOnly(true).queue();
+
     }
 
 }
